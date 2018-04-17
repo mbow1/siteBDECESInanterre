5865ea9c42195efe92904ab98a0895dfa00acfa8
Doddollinno <dorian.theriez@gmail.com>
2018-04-17 11:41:04 +0200

    BDD FONCTIONNELLE ༼ つ ͡ ͡° ͜ ʖ ͡ ͡° ༽つ

--- inscripConnex.php
+++ inscripConnex.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
     try{
-            $pdo = new PDO('mysql:host=localhost;dbname=BDD;', 'root', '');
+            $pdo = new PDO('mysql:host=localhost;dbname=bdd;', 'root', 'exia');
         }
         catch (PDOException $e){
             echo $e->getMessage();
