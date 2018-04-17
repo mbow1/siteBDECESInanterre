ec8706d62521416af73160de6e0fd7216fff6802
mbow1 <morniass.mbow@viacesi.fr>
2018-04-17 09:55:19 +0200

    connection BBD

--- inscripConnex.php
+++ inscripConnex.php
@@ -1,17 +1,69 @@
 <?php
-
+session_start();
     try{
             $pdo = new PDO('mysql:host=localhost;dbname=BDD;', 'root', '');
         }
         catch (PDOException $e){
             echo $e->getMessage();
+        }
 
+        if (isset($_POST['ValiderInscription'])){
+
+                $nom = htmlspecialchars($_POST['nom']);
+                $mail = htmlspecialchars($_POST['email']);
+                $prenom = htmlspecialchars($_POST['prenom']);
+                $passe = sha1($_POST['passe']);
+                $passe2 = sha1($_POST['passe2']);
+
+            if(!empty($_POST['nom']) and !empty($_POST['prenom']) and !empty($_POST['passe']) and !empty($_POST['passe2']) and !empty($_POST['email'])){
+
+
+                $nomlength = strlen($nom);
+                if ($nomlength <= 25){
+                    $prenomlength = strlen($prenom);
+                        if ( $prenomlength <= 25){
+                            /// condition pour adresse mail du CESI
+                            if( filter_var($mail, FILTER_VALIDATE_EMAIL)){
+                                $reqmail = $pdo->prepare("SELECT * FROM user WHERE AdresseMail_User = ?");
+                                $reqmail->execute(array($mail));
+                                $mailexist = $reqmail->rowCount();
+                                 if ($mailexist == 0){
+                                        if ( $passe == $passe2){
+
+                                         ////////requete /////
+                                        $inseruser = $pdo->prepare("INSERT INTO user(Nom_User, Prenom_User,AdresseMail_User,MotDePasse_User) VALUES (?, ?, ?, ?)");
+                                        $inseruser->execute(array($nom,$prenom,$mail,$passe));
+                                        /////////requete ///////////
+                                        $messageBon = "Nickel, compte créé ";
+                                } else {
+                                   $message = "Vos mots de passe ne correspondent pas !!";
+                                }
+                                 } else {
+                                     $message = "Adresse mail déjà utiliser";
+                                 }
+                            } else {
+                                $message = "veuillez renseigner un email valide !";
+                            }
+                        }else{
+                            $message = "Votre prénom est trop long !";
+                        }
+                } else{
+                    $message = "Votre nom est trop long !";
+                }
+
+            } else {
+                $message = "Tous les champs doivent être complétés !";
+            }
         }
 ?>
 <!DOCTYPE html>
 <html>
     <head>
   <!--****************************** import ***************************************!-->
+
+
+
+
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
@@ -36,8 +88,9 @@
         </button>
 
 
-        <div class="form-group " id="conect">
-            <form method="post" class="form-horizontal" >
+
+        <div class="form-group " id="conect" >
+            <form method="POST" class="form-horizontal" action="" >
                 <img class="logocesi" src="img/cesi-entreprise.png">
                 <input type="email" name="mail" class="form-control" placeholder="Entrer votre mail CESI"/><br/>
                 <input type="password" name="motDePasse" class="form-control" placeholder="Mots de Passe" /><br/>
@@ -45,31 +98,40 @@
                     <label><input type="checkbox"> Se souvenir de moi !</label>
                 </div>
                 <br/>
-                <a type="submit" name="valider" value="Valider" class=" btn btn-primary" href="http://localhost/ProjetWeb/market.php"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Valider</a><br/>
+                <input type="submit" name="valider" value="Se Connecter" class=" btn btn-primary" href="http://localhost/ProjetWeb/market.php"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></input><br/>
 
             </form>
         </div>
         <div id="FormulDinscription" class="form-group ">
-            <form method="post" class="form-horizontal">
+            <form method="POST" class="form-horizontal" action="">
 
                 <input type="text" name="nom" class="form-control" placeholder="Votre Nom"/><br/>
-                <input type="text" name="prenom" class="form-control" placeholder="Votre Prenom"/><br/>
+                <input type="text" name="prenom" class="form-control" placeholder="Votre Prenom" /><br/>
                 <input type="password" name="passe" class="form-control" placeholder="Votre Mot de passe"/><br/>
                 <input type="password" name="passe2" class="form-control" placeholder="Confirmation du mot de passe"/><br/>
-                <input type="text" name="email" class="form-control" placeholder="Adresse e-mail CESI"/><br/>
-                <a type="submit" class="btn btn-primary" href="http://localhost/ProjetWeb/market.php">
-                <span class="glyphicon glyphicon-ok"></span> Valider
-                </a>
+                <input type="email" name="email" class="form-control" placeholder="Adresse e-mail CESI"/><br/>
+
+                <input type="submit" name="ValiderInscription" class="btn btn-primary"  value="M'inscrire" >
+                <span class="glyphicon glyphicon-ok"></span>
+                </input>
                 <br/><br/>
 
                 <?php
-                $message = "Votre Mot de passe n'est pas bon !";
-                echo '<div class="alert alert-danger" role="alert">
-                  <a href="#" class="alert-link">'.$message.'</a>
-                </div>';
+
+                    if(isset($message)){
+                        echo '<div class="alert alert-danger" role="alert">
+                            <a href="#" class="alert-link">'.$message.'</a>
+                            </div>';
+                    }
+                    if(isset($messageBon)){
+                        echo '<div class="alert alert-success" role="alert">
+                            <a href="#" class="alert-link">'.$messageBon.'</a>
+                            </div>';
+                    }
 
 
                 ?>
+
             </form>
 
         </div>
