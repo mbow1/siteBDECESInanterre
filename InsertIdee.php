3b3a1fc864f52e85eb7cdadf8682bb73680166e9
DylanBC <dylan----2011@hotmail.fr>
2018-04-18 17:32:25 +0200

    voici insert boite idée

---
+++ InsertIdee.php
@@ -0,0 +1,7 @@
+INSERT INTO boiteidee (Titre_Idee, Description_Idee)
+	VALUES
+    ('Après-midi LAN', 'Venez au CESI jeudi 8 février pour une grande après midi LAN ! Concours de FIFA et Fortnite sont attendus'),
+    ('Club dramatique', 'Le club d\'art dramatique du CESI nanterre se rassemble à nouveau tout le monde est bienvenue ! Venez nombreux !'),
+    ('Tournoi de foot', 'Venez tater du ballon rond durant toute une après-midi pour gagner un panini chez Ali'),
+    ('Initiation Jeu de rôle', 'Le club JdR est disposé à vous accueillir tout les jeudis pour des aventures passionantes ! Ce jeudi le club JdR ouvrira ses portes avec des sessions plus courtes que les sessions habituelles afin de vous faire découvrir le monde du JdR dans ');
+
